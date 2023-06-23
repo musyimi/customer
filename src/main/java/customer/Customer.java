b0039737399023e38f16d466f8e24ed8286c3d03
musyimi <musyimi13@gmail.com>
2023-06-23 16:22:34 +0300

    added entity annotations to map the customer class to the postgres database

--- src/main/java/customer/Customer.java
+++ src/main/java/customer/Customer.java
@@ -2,9 +2,24 @@ package customer;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 
+@Entity
  public class Customer{
+	@Id
+	@SequenceGenerator(
+			name = "customer_id_sequence",
+			sequenceName = "customer_id_sequence"
+			)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "customer_id_sequence"
+			)
 	private Integer id;
 	private String first_name;
 	private String last_name;
