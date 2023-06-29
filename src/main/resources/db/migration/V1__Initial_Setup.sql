de420a97670442982dcc0e752ff9f3457ec21522
musyimi <musyimi13@gmail.com>
2023-06-29 10:43:42 +0300

    updated db migration from BIGSERIAL to Int

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customer (
-   id INT DEFAULT nextval('customer_id_seq') PRIMARY KEY,
+   id INT PRIMARY KEY,
    first_name TEXT not null,
    last_name TEXT NOT NULL,
    email TEXT UNIQUE NOT NULL
